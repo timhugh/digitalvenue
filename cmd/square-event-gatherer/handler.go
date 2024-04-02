9d33db11f7d82fa58ba07574654d638d5d1c0fc4
Tim Heuett <tim.heuett@gmail.com>
2024-04-02 14:23:58 -0700

    event gatherer as echo service

--- cmd/square-event-gatherer/handler.go
+++ cmd/square-event-gatherer/handler.go
@@ -14,9 +14,9 @@ func newHandler(log zerolog.Logger) handler {
 	return handler{log: log}
 }
 
-func (handler handler) handle(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func (handler handler) handle(request events.SQSEvent) (events.SQSEventResponse, error) {
 
 	fmt.Printf("Received request: %v\n", request)
 
-	return events.APIGatewayProxyResponse{}, nil
+	return events.SQSEventResponse{}, nil
 }
--- cmd/square-event-gatherer/handler_test.go
+++ cmd/square-event-gatherer/handler_test.go
@@ -3,33 +3,26 @@ package main
 import (
 	"github.com/matryer/is"
 	"github.com/rs/zerolog"
-	"os"
 	"testing"
 
 	"github.com/aws/aws-lambda-go/events"
 )
 
-var webhookEventRawJSON, _ = os.ReadFile("test-event.json")
-var webhookEventJSON = string(webhookEventRawJSON)
-
-const goodSignature = "/p9MrQ6sTzL2iuGBPa5YoadntDIMv5ms+ihDe3MLoLc="
-
 func TestHandler(t *testing.T) {
 	testCases := []struct {
-		name string
-		// given
-		request events.APIGatewayProxyRequest
-		// then
-		expectedStatus int
-		expectedBody   string
+		name             string
+		request          events.SQSEvent
+		expectedResponse events.SQSEventResponse
 	}{
 		{
 			name: "basic success",
-			request: events.APIGatewayProxyRequest{
-				Body: webhookEventJSON,
+			request: events.SQSEvent{
+				Records: []events.SQSMessage{
+					{
+						Body: `{"type": "payment.created"}`,
+					},
+				},
 			},
-			expectedStatus: 200,
-			expectedBody:   `{"status": "success"}`,
 		},
 	}
 
@@ -43,8 +36,7 @@ func TestHandler(t *testing.T) {
 
 			response, err := handler.handle(testCase.request)
 			is.NoErr(err)
-			is.Equal(response.StatusCode, testCase.expectedStatus)
-			is.Equal(response.Body, testCase.expectedBody)
+			is.Equal(testCase.expectedResponse, response)
 		})
 	}
 }
