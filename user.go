9373096f76a36712e350c4ee38bc88082b0b64c2
Tim Heuett <tim.heuett@gmail.com>
2025-03-23 08:24:18 -0700

    reset

--- user.go
+++
@@ -1,8 +0,0 @@
-package dv
-
-type User struct {
-	UUID         string `json:"uuid" db:"uuid"`
-	Name         string `json:"name" db:"name"`
-	Username     string `json:"username" db:"username"`
-	PasswordHash string `json:"password_hash" db:"password_hash"`
-}
