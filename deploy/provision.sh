ea09cc50043d340f9fb62d6eb46afc202aa43410
Tim Heuett <tim.heuett@gmail.com>
2025-03-25 01:18:57 -0700

    add nginx to deployment

--- deploy/provision.sh
+++ deploy/provision.sh
@@ -2,11 +2,32 @@
 
 set -e
 
+required_environment_variables="DOMAIN_NAME IP_ADDRESS APP_VERSION"
+for variable in $required_environment_variables; do
+  if [ -z "${!variable}" ]; then
+    echo "Error: $variable is not set"
+    exit 1
+  fi
+done
+
 # Wait for cloud-init to finish
 cloud-init status --wait || true
 
 # Install java
-apt-get update && apt-get install -y openjdk-21-jre-headless
+apt-get update
+apt-get install -y \
+  openjdk-21-jre-headless \
+  nginx
+
+# Set up environment variables
+envsubst < /opt/env.template > /opt/dvserver/.env
+
+# Install nginx configuration
+envsubst < /opt/nginx.conf.template > /etc/nginx/sites-available/digital-venue
+ln -s /etc/nginx/sites-available/digital-venue /etc/nginx/sites-enabled/digital-venue
+rm /etc/nginx/sites-enabled/default
+nginx -t
+systemctl restart nginx
 
 # Install service definition for digital-venue server
 cp /opt/digital-venue.service /etc/systemd/system/digital-venue.service
