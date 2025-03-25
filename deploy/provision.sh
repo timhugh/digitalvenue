553ca16e266d010ff6d5df9ccd6661e526bcf760
Tim Heuett <tim.heuett@gmail.com>
2025-03-25 01:34:15 -0700

    supply all env variables; provision on each deploy

--- deploy/provision.sh
+++ deploy/provision.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+# NOTE: this script is used to provision new servers but it is also
+# run again on every deployment, so everything in here must be idempotent.
+
 set -e
 
-required_environment_variables="DOMAIN_NAME IP_ADDRESS APP_VERSION"
+required_environment_variables="DOMAIN_NAME IP_ADDRESS APP_VERSION ENVIRONMENT"
 for variable in $required_environment_variables; do
   if [ -z "${!variable}" ]; then
     echo "Error: $variable is not set"
@@ -24,14 +27,17 @@ envsubst < /opt/env.template > /opt/dvserver/.env
 
 # Install nginx configuration
 envsubst < /opt/nginx.conf.template > /etc/nginx/sites-available/digital-venue
-ln -s /etc/nginx/sites-available/digital-venue /etc/nginx/sites-enabled/digital-venue
-rm /etc/nginx/sites-enabled/default
+ln -sf /etc/nginx/sites-available/digital-venue /etc/nginx/sites-enabled/digital-venue
+rm -f /etc/nginx/sites-enabled/default
 nginx -t
 systemctl restart nginx
 
 # Install service definition for digital-venue server
 cp /opt/digital-venue.service /etc/systemd/system/digital-venue.service
 systemctl enable digital-venue.service
+
+# Reload systemd daemon to pick up changes
+systemctl daemon-reload
 
 # Create directory for deployments
 mkdir -p /opt/dvserver/versions
