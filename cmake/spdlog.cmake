c4952245f99f2516f94025d8468e74a51a06b394
Tim Heuett <tim.heuett@gmail.com>
2025-03-30 20:10:41 -0700

    reset

--- cmake/spdlog.cmake
+++
@@ -1,6 +0,0 @@
-include(FetchContent)
-FetchContent_Declare(spdlog
-  GIT_REPOSITORY https://github.com/gabime/spdlog.git
-  GIT_TAG v1.15.2
-)
-FetchContent_MakeAvailable(spdlog)
--- server/tests/test_main.cpp
+++
@@ -1,4 +0,0 @@
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch_test_macros.hpp>
-
-TEST_CASE("tests are working") { REQUIRE(true); }
